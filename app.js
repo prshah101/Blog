2f6ca21be4d4da17665c6d5d555d7676f7283fdc
Ria <62290817+prshah101@users.noreply.github.com>
2022-03-23 10:29:25 +1100

    integrated old files

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const port = 3000
 
-app.get("/", function(req,res){
-res.send("Working")
-})
+app.use(express.urlencoded({ extended: true }));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use(express.static('css'));
+
+let viewsPath = __dirname + "/pages/";
+
+app.get("/", (req, res) => {
+    let fileName = viewsPath + "index.html";
+    res.sendFile(fileName);
+});
+
+app.get("/blog", (req, res) => {
+  let fileName = viewsPath + "/blog/blogposts.html";
+  res.sendFile(fileName);
+});
+
+app.get("/UniTips", (req, res) => {
+  let fileName = viewsPath + "/blog/unitips.html";
+  res.sendFile(fileName);
+});
+
+app.get("/motorbike", (req, res) => {
+  let fileName = viewsPath + "/blog/motorbike.html";
+  res.sendFile(fileName);
+});
+
+
+app.get("*", (req, res) => {
+  let fileName = viewsPath + "404.html";
+  res.sendFile(fileName);
+});
 
 app.listen(process.env.PORT || port, () => { //listen to whatever port is avilable on heroku
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port ${port}`)
+})
